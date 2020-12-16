93336cc410f4f9db55652e4ec02b2d36ca4c74ee
bellagented <bellagented@gmail.com>
2020-12-16 11:22:50 +0100

    login con auth0

---
+++ src/Pages/LogIn.js
@@ -0,0 +1,6 @@
+import React from "react";
+import LoginButton from '../components/login-button';
+export default function Home(){
+return(<div><h1>Login</h1>
+<LoginButton/></div>)
+}
---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url('https://i.pinimg.com/originals/44/9e/05/449e058844d89d45822a44c9f5f60fe2.gif')",
+        width: "100%",
+        height: "100vh",
+        backgroundRepeat: 'no-repeat',
+        margin:'50px'
+      }}
+    >
+      LOADING...
+    </div>
+  );
+}
