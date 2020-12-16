6849cf09d80c1714267bb4acca667d5dd8346f3b
bellagented <bellagented@gmail.com>
2020-12-16 16:02:48 +0100

    rimosso signup

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -1,8 +1,8 @@
 import React from "react";
 import LoginButton from '../components/login-button';
-import SignupButton from '../components/signup-button';
+
 export default function Home(){
 return(<div><h1>Login</h1>
 <LoginButton/>
-<SignupButton/></div>)
+</div>)
 }
