c8086cff1bd7b412e9e66c2d047f2648f7806107
bellagented <bellagented@gmail.com>
2020-12-21 23:18:38 +0100

    commenti post in home

--- src/HomeComponent/SeePost.js
+++ src/HomeComponent/SeePost.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import CommentSection from '../components/comment-section'
 const postprova = [
   {
+    id:'mklmKkflkm',
     from: "Valero",
     type: "post",
     text: "Ultimo livello finito! ",
     img:
       "https://images.unsplash.com/photo-1585671962215-473bcfa06caa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=771&q=80",
+    comment: [],
   },
   {
+    id:'sflkIKML',
     from: "RizzoRules",
     type: "review",
     game: "Cyberpunk2077",
     text: "ridatemi i miei soldi",
+    comment: [],
   },
 ];
 export default function SeePost() {
@@ -26,13 +32,13 @@ export default function SeePost() {
       return props.posts.map((post, id) => {
         return post.type === "post" ? (
           <div key={id}>
-            <PostLayout post={post} />
-            <hr className='line'></hr>
+            <PostLayout post={post} allPost={allPost} setAllPost={setAllPost} />
+            <hr className="line"></hr>
           </div>
         ) : (
           <div key={id}>
-            <ReviewLayout review={post} />
-            <hr className='line'></hr>
+            <ReviewLayout review={post} allPost={allPost} setAllPost={setAllPost} />
+            <hr className="line"></hr>
           </div>
         );
       });
@@ -49,7 +55,7 @@ export default function SeePost() {
 }
 
 export function PostLayout(props) {
-  const { from, text, img } = props.post;
+  const { from, text, img, comment, id } = props.post;
   return (
     <div className="borderbox m-10">
       <div className="p-10">
@@ -57,18 +63,21 @@ export function PostLayout(props) {
         <p>{text}</p>{" "}
       </div>
       <img src={img} alt="post" style={{ width: "100%" }} />
+      <CommentSection comment={comment} id={id} allPost={props.allPost} setAllPost={props.setAllPost}/>
     </div>
   );
 }
 
 export function ReviewLayout(props) {
-  const { from, text, game } = props.review;
+  const { from, text, game, comment, id } = props.review;
   return (
     <div className="borderbox m-10 p-10">
       <h3 className="title">
         {from} reviewed {game}
       </h3>
       <p>{text}</p>
+      <CommentSection comment={comment} id={id} allPost={props.allPost} setAllPost={props.setAllPost} />
     </div>
   );
 }
+
---
+++ src/components/comment-section.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function CommentSection(props) {
+  const { user } = useAuth0();
+  const { name } = user;
+  const [comment, setComment] = useState("");
+
+  const addComment = (e) => {
+    e.preventDefault();
+    const i = props.allPost.findIndex((post) => {
+      return post.id === props.id;
+    });
+    const post = props.allPost.slice();
+    post[i].comment.push({ from: name, text: comment });
+    props.setAllPost(post);
+  };
+
+  return (
+    <div>
+      <button>Comment</button>
+      {props.comment.map((comm, id) => {
+        return (
+          <div key={id}>
+            <h3>{comm.from}</h3>
+            <p>{comm.text}</p>
+          </div>
+        );
+      })}
+      <form onSubmit={addComment}>
+        <input
+          type="text"
+          placeholder="comment"
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+        />
+        <button onClick={addComment}>send</button>
+      </form>
+    </div>
+  );
+}
